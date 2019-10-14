7e3ef5051b6fc35d393a1b719876504660b3f4f8
Herman Liu <hermanliu95@gmail.com>
2019-10-14 16:03:49 -0700

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,91 @@
-//Beginning of game, it should show the heading
-//And a start button to start the game,
-//CLick start, the counter starts clicking down, 
-//Timer should be set to around 120 seconds.
-//On the page of the timer, create about 10 questions,
-//Create an array of question objects
-//with a click function to select the answers.
-//On the bottom of the page, there should be a done button
-//It should say correct answers:
-//incorrect answers:
-//Unanswered:
-
 $(document).ready(function(){
+
+//Creating Questions
+let questions = [
+    {
+        question: 'What does HTML stand for?',
+        choiceA: 'Hyper Text Markup Language',
+        choiceB: 'Home Tool Markup Language',
+        choiceC: 'Hyperlinks and Text Markup Language',
+        choiceD: 'Blue',
+        answer: 'A'
+    },
+    {
+        question: "Choose the correct HTML tag for the largest heading:",
+        choiceA: 'Head',
+        choiceB: 'H1',
+        choiceC: 'H6' ,
+        choiceD: 'Heading',
+        answer: 'B'
+    },
+    {
+        question: "What is the correct HTML tag for inserting a line break?",
+        choiceA: 'break',
+        choiceB: 'br /br',
+        choiceC: 'lb',
+        choiceD: 'br/',
+        answer: 'D'
+    },
+    {
+        question: "Choose the correct HTML tag to make a text bold  ",
+        choiceA: 'bold',
+        choiceB: 'b',
+        choiceC: 'bb',
+        choiceD: 'strong',
+        answer: 'A'
+        
+    },
+    {
+        question: "What does CSS stand for?",
+        choiceA: 'Creative Style Sheets',
+        choiceB: 'Colorful Style Sheets',
+        choiceC: 'Computing Style Sheets',
+        choiceD: 'Cascading Style Sheets',
+        answer: 'D'
+    },
+    {
+        question: "How do you insert a comment in a CSS file?",
+        choiceA: "'this is a comment" ,
+        choiceB: '/*This is a comment */',
+        choiceC: '//This is a comment',
+        choiceD: '//This is a comment. //',
+        answer: 'B'
+    },
+    {
+        question: "How do you change the text color of an element?",
+        choiceA: 'Color:',
+        choiceB: 'Text-Color:',
+        choiceC: 'Text-Color = ',
+        choiceD: 'Fgcolor:',
+        answer: 'A'
+        
+    },
+    {
+        question: "Who created Javascript?",
+        choiceA: 'Microsoft',
+        choiceB: 'Sun Microsystems',
+        choiceC: 'Netscape',
+        choiceD: 'Oracle' ,
+        answer: 'C'
+    },
+    {
+        question: "What was Javascript called before it was ever called Javascript?",
+        choiceA: 'Latte',
+        choiceB: 'Mocha',
+        choiceC: 'Livescript',
+        choiceD: 'BScript',
+        answer: 'C'
+    },
+    {
+        question: "What type of programming langauge is Javascript?",
+        choiceA: 'Front End',
+        choiceB: 'Back End',
+        choiceC: 'Full-Stack',
+        choiceD: 'None',
+        answer: 'C'
+    },
+]
+
 
 
 
