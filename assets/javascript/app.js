81d25f82eba7d3e968b29c31d2e003a9bc30b21c
Herman Liu <hermanliu95@gmail.com>
2019-10-14 15:09:07 -0700

    second commit, styled page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+//Beginning of game, it should show the heading
+//And a start button to start the game,
+//CLick start, the counter starts clicking down, 
+//Timer should be set to around 120 seconds.
+//On the page of the timer, create about 10 questions,
+//Create an array of question objects
+//with a click function to select the answers.
+//On the bottom of the page, there should be a done button
+//It should say correct answers:
+//incorrect answers:
+//Unanswered:
+
+$(document).ready(function(){
+
+
+
+
+
+});
